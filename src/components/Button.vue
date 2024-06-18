1106183e366e33ed9fa71d2cadb9815f53f160cb
MMDREZA <mheidari831003.game@gmail.com>
2024-06-18 17:02:44 +0330

    ui task tracker started, created three buttons and one h1 with task tracker title

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button :style="{ background: color }" class="btn">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    title: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button title="Add Task" color="green" />
+    <Button title="Update Task" color="grey" />
+    <Button title="Remove Task" color="red" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    buttonTitle: String,
+  },
+  components: { Button },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 20px;
+}
+</style>
